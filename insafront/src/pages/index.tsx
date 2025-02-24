aac2571475c65f1508b2f383d886bcf05c9b73d5
kyusang0330 <cholongs123@naver.com>
2025-02-24 16:40:06 +0900

    first commit

---
+++ insafront/src/pages/index.tsx
@@ -0,0 +1,17 @@
+
+import TopBar from "@/component/topbar/Topbar";
+import SignupForm from "@/component/SignupForm";
+
+
+
+
+
+export default function Home() {
+  return (
+    <>
+      <TopBar />
+        <h1>index index</h1>
+      {/*<SignupForm />*/}
+    </>
+  );
+}
---
+++ insafront/src/type/mytype.ts
@@ -0,0 +1,7 @@
+import {ReactNode} from "react";
+//사용자정의 타입을 정의하고 내보내기
+
+export interface LayoutProps{
+    children : ReactNode;
+    children2 : ReactNode;
+}
