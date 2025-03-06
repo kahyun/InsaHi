29f6df3f9334c1b9f48b7158d6f72495ef14199e
Kim Daul <elre519@naver.com>
2025-03-06 16:29:19 +0900

    Sal add

--- User/src/main/java/com/playdata/User/employee/dao/EmployeeDAOImpl.java
+++ User/src/main/java/com/playdata/User/employee/dao/EmployeeDAOImpl.java
@@ -4,7 +4,6 @@ package com.playdata.User.employee.dao;
 import com.playdata.User.employee.entity.Employee;
 import com.playdata.User.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
