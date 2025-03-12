ebedf71768b2019e6f461326222b23d6f3642f16
jahee24 <8dhfflavlr8@naver.com>
2025-03-12 17:22:13 +0900

    feature:leave POST /api/annual-leave/submit: 휴가 신청 GET /api/annual-leave/usage/{employeeId}: 직원의 휴가 신청 내역 조회 GET /api/annual-leave/usage/company/{companyCode}/{status}: 회사 내 특정 상태의 휴가 신청 내역 조회 PUT /api/annual-leave/approve: 휴가 승인 PUT /api/annual-leave/reject: 휴가 반려 PUT /api/annual-leave/approve/additional: 추가 연차 승인 @scheduled(cron = "0 0 0 1 * ?") 휴가 등록 - 매달 발생

--- AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDao/sal/AllowanceDao.java
+++ AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDao/sal/AllowanceDao.java
@@ -2,15 +2,18 @@ package com.playdata.attendanceSalary.atdSalDao.sal;
 
 import com.playdata.attendanceSalary.atdSalEntity.sal.AllowanceEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface AllowanceDao {
-     AllowanceEntity saveAllowance(AllowanceEntity allowanceEntity);
-    Optional<AllowanceEntity> findAllowanceById(Long id);
-    void deleteAllowanceById(Long id);
 
+  AllowanceEntity saveAllowance(AllowanceEntity allowanceEntity);
 
+  Optional<AllowanceEntity> findAllowanceById(Long id);
 
+  void deleteAllowanceById(Long id);
 
+  //김재희
+  List<AllowanceEntity> findByPayStubId(Long payStubId);
 }
--- AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDao/sal/AllowanceDaoImpl.java
+++ AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDao/sal/AllowanceDaoImpl.java
@@ -2,6 +2,7 @@ package com.playdata.attendanceSalary.atdSalDao.sal;
 
 import com.playdata.attendanceSalary.atdSalEntity.sal.AllowanceEntity;
 import com.playdata.attendanceSalary.atdSalRepository.sal.AllowanceRepository;
+import java.util.List;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,24 +12,31 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Repository
 public class AllowanceDaoImpl implements AllowanceDao {
-    private AllowanceRepository allowanceRepository;
 
-    @Override
-    public AllowanceEntity saveAllowance(AllowanceEntity allowanceEntity) {
-        allowanceRepository.save(allowanceEntity);
-        return allowanceEntity;
-    }
+  private AllowanceRepository allowanceRepository;
+
+  @Override
+  public AllowanceEntity saveAllowance(AllowanceEntity allowanceEntity) {
+    allowanceRepository.save(allowanceEntity);
+    return allowanceEntity;
+  }
 
 
-    @Override
-    public Optional<AllowanceEntity> findAllowanceById(Long id) {
-        return allowanceRepository.findById(id);
-    }
+  @Override
+  public Optional<AllowanceEntity> findAllowanceById(Long id) {
+    return allowanceRepository.findById(id);
+  }
 
-    @Override
-    public void deleteAllowanceById(Long id) {
-        allowanceRepository.deleteById(id);
-        allowanceRepository.flush();
+  @Override
+  public void deleteAllowanceById(Long id) {
+    allowanceRepository.deleteById(id);
+    allowanceRepository.flush();
 
-    }
+  }
+
+  //김재희
+  @Override
+  public List<AllowanceEntity> findByPayStubId(Long payStubId) {
+    return List.of();
+  }
 }
