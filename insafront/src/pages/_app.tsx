1f788d33e0c64b8cd443ca7f38b51b9dcc878dc9
kyusang0330 <cholongs123@naver.com>
2025-03-17 19:38:59 +0900

    Logout 버튼 && 정리

--- insafront/src/pages/_app.tsx
+++ insafront/src/pages/_app.tsx
@@ -1,8 +1,35 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import MainLayout from "@/component/MainLayout";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+
+    const router = useRouter();
+
+    // 로그인과 회원가입 페이지에서는 MainLayout을 사용하지 않음
+    const noLayoutPaths = ["/Login", "/SignupForm","/"];
+    const isNoLayoutPage = noLayoutPaths.includes(router.pathname);
+
+    useEffect(() => {
+        const publicPaths = ["/Login", "/SignupForm","/"]; // 인증 없이 접근 가능한 페이지
+        const pathIsPublic = publicPaths.includes(router.pathname);
+
+        const token = localStorage.getItem("accessToken");
+
+        if (!token && !pathIsPublic) {
+            // window.location.href ="/"
+            router.push("/"); // 로그인 안 했으면 로그인 페이지로 이동
+        }
+    }, [router.pathname]);
+
+
+  return isNoLayoutPage ? (
       <Component {...pageProps} />
+  ) : (
+      <MainLayout>
+          <Component {...pageProps} />
+      </MainLayout>
   );
 }
