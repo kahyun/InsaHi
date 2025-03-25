45d00af72dbee98ca31f83cccd4c63fd145e8d72
Kim Daul <elre519@naver.com>
2025-03-25 16:39:12 +0900

    atdsal

--- Gateway/src/main/java/com/playdata/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/playdata/Gateway/GatewayApplication.java
@@ -1,4 +1,5 @@
 package com.playdata.Gateway;
+//1
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
 
-    }
+  }
 }
